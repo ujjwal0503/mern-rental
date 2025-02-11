f7ac6d536c9ebbf2a862cec682fa1f610dcce44d
ujjwal0503 <ujjwal050503@gmail.com>
2025-02-11 22:14:26 +0530

    user model creation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,17 +21,17 @@ export default function Header() {
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='font-bold hidden sm:inline text-slate-700 hover:underline'>
               Home
             </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='font-bold hidden sm:inline text-slate-700 hover:underline'>
               About
             </li>
           </Link>
           <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'> Sign in</li>
+            <li className='font-bold text-slate-700 hover:underline'> Sign in</li>
           </Link>
        
         </ul>
