97f74b4c19838d70d0c1b46679a214a3160b8698
ujjwal0503 <ujjwal050503@gmail.com>
2025-02-12 17:53:44 +0530

    update header and profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,28 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Profile from './pages/Profile';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import About from './pages/about';
 import SignIn from './pages/Sign-In';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
-    
     <BrowserRouter>
-    <Header />
-    <Routes>
+      <Header />
+      <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
-    </Routes>
+
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
